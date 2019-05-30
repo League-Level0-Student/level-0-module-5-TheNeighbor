365708efc7aca18e2bf0aa269653db0840fa8bb2
league <league@imac.attlocal.net>
2019-05-29 17:01:35 -0700

    For Loop Gauntlet: IP :smile:

--- src/algorithms/PrimeOrNot.java
+++ src/algorithms/PrimeOrNot.java
@@ -6,16 +6,15 @@ public class PrimeOrNot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String number = JOptionPane.showInputDialog("Type in a number.");
-int num = Integer.parseInt(number);
-for (int i = 2; i < num; i++) {
-	if (num % i==0) {
-		JOptionPane.showMessageDialog(null, num + " is not prime.");
-		System.exit(0);
-	}
-	}
-JOptionPane.showMessageDialog(null, num + " is prime.");
+		String number = JOptionPane.showInputDialog("Type in a number.");
+		int num = Integer.parseInt(number);
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				JOptionPane.showMessageDialog(null, num + " is not prime.");
+				System.exit(0);
+			}
+		}
+		JOptionPane.showMessageDialog(null, num + " is prime.");
 	}
 
 }
-                        
--- src/extra/ForLoopGauntlet.java
+++ src/extra/ForLoopGauntlet.java
@@ -4,31 +4,37 @@ public class ForLoopGauntlet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for (int i = 0; i < 101; i++) {
-	System.out.println(i);
-}
-for (int i = 100; i > -1; i--) {
-	System.out.println(i);
-}
-for (int i = 2; i < 101; i++) {
-	if (i%2 == 0) {
-		System.out.println(i);
-	}
-}
-for (int i = 1; i < 100; i++) {
-	if (i%2 == 1) {
-		System.out.println(i);
-	}
-}
-for (int i = 1; i < 101; i++) {
-	if(i%2 == 0) {
-		System.out.println(i + " is even");
-	}
-	else {
-		System.out.println(i + " is odd");
-	}
-	
-}
+		for (int i = 0; i < 101; i++) {
+			System.out.println(i);
+		}
+		System.out.println();
+		for (int i = 100; i > -1; i--) {
+			System.out.println(i);
+		}
+		System.out.println();
+		for (int i = 2; i < 101; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println();
+		for (int i = 1; i < 100; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println();
+		for (int i = 1; i < 101; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+		System.out.println();
+		for (int i = 0; i <= 777; i += 7) {
+			System.out.println(i);
+		}
+		System.out.println();
+		for (int i = 2005; i < 2020; i++) {
+			System.out.println("In " + i + ", I was " + (i - 2005) + " year(s) old.");
+		}
 	}
 
 }
