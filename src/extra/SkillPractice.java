26354dcd3977ccde155a9869c4b1cd3b86bf2f84
league <league@imac-18.attlocal.net>
2019-07-11 20:03:43 -0700

    Skill Practice :smile:

--- src/extra/SkillPractice.java
+++ src/extra/SkillPractice.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 SkillPractice skills = new SkillPractice();
           	//skills.skill1();
           	//skills.skill2();
-          	skills.skill3();
+          	//skills.skill3();
           	skills.skill4();
           	skills.skill5();
 }
@@ -56,23 +56,31 @@ for (int i = 1; i < 31; i+=3) {
 
 void skill3() { // Get a random number that is less than 20 and print it to the console 
 Random randy = new Random();
-System.out.println(randy.nextInt(20));
+int randint = randy.nextInt(20);
+System.out.println(randint);
 // Get another random number that is less than 10 and print it to the console 
-
+Random robot = new Random();
+int robint = robot.nextInt(10);
+System.out.println(robint);
 
 
 // Using a pop-up, tell the user the difference between the numbers // Hint: use subtraction 
-
+JOptionPane.showMessageDialog(null, (randint-robint));
 
 
 }
 
 void skill4() { // In a pop-up, ask the user for the city they live in 
-
+String city = JOptionPane.showInputDialog("What city do you live in?");
 
 
 // If they answered "San Diego", tell them they live in America's Finest City 
-
+if (city.equalsIgnoreCase("San Diego")) {
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City.");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Move to San Diego.");
+}
 
 
 // Otherwise, tell them to move to San Diego 
@@ -80,7 +88,13 @@ void skill4() { // In a pop-up, ask the user for the city they live in
 
 
 // Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
-
+int cars = 3;
+if (cars == 0) {
+	JOptionPane.showMessageDialog(null, "I bet you use public transportation.");
+}
+else {
+	JOptionPane.showMessageDialog(null, "");
+}
 
 
 // If there is 1 car, use a pop-up to display the make/model of the car 
@@ -94,11 +108,11 @@ void skill4() { // In a pop-up, ask the user for the city they live in
 }
 
 void skill5() { // In a pop-up, ask the user for the name of their school 
-
+String school = JOptionPane.showInputDialog("What is the name of your school?");
 
 
 // In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message. 
-
+JOptionPane.showMessageDialog(null, school + " is a fantastic school.");
 
 
 }
