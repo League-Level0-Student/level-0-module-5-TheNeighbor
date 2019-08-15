7710e416c28d8d7d4f2fd07b68b6ff0d7ff0157f
league <league@imac-14.attlocal.net>
2019-08-14 18:28:41 -0700

    Practice for test.

---
+++ src/extra/Practice.java
@@ -0,0 +1,19 @@
+package extra;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String question = JOptionPane.showInputDialog("Are you guilty of the crime?");
+		
+	}
+	static void jury() {
+		int decision = new Random().nextInt(2);
+	}
+	}
+
+
